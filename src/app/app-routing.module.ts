b1fda8ab1f6cab1654d1ff555a36edcd8c4798de
ux_consultor@mdp.com.pe-at-957159725455 <ascolanr@gmail.com>
2022-12-06 14:36:37 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './views/index/index.component';
+import { MensajeComponent } from './views/mensaje/mensaje.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '#', pathMatch: 'full' },
+  { path: 'index', component: IndexComponent},
+  { path: 'mensaje', component: MensajeComponent},
+  { path: '**', component: IndexComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
